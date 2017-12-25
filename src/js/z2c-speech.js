58bb3c2c63ecd34b97cb56d214f8a68af5b6b3bc
adambodie <adam.bodie@yahoo.com>
2017-12-25 11:42:01 -0800

    Added translate button, click functionality to convert text to Pig Latin

--- src/js/z2c-speech.js
+++ src/js/z2c-speech.js
@@ -2,48 +2,67 @@
 ** z2c-speech.js
 */
 
-function initPage () {
-  const _mic = $('#microphone'); const _stop = $('#stop'); const _reset = $('#reset'); const _speech = $('#speech');
-    _mic.addClass("mic_enabled");
-    _stop.addClass("mic_disabled");
-    _reset.addClass("mic_enabled");
-    
-  _reset.on("click", () => {
-      _mic.addClass("mic_enabled");
-      _mic.removeClass("mic_disabled");
-	  const resetText = _speech.text().replace(/.*/, "Spoken output goes here");
-	  _speech.html(resetText);
-	  console.log("Speech cleared");
-   });
-  _mic.on("click", function() {
-      var _className = this.className;
-      if(this.className == "mic_enabled") {
-        _mic.addClass("mic_disabled");
-        _mic.removeClass("mic_enabled");
-        _stop.addClass("mic_enabled");
-        _stop.removeClass("mic_disabled");
-        $.when($.get('/api/speech-to-text/token')).done(
-          function (token) {
+initPage = () => {
+	const _mic = $('#microphone'), 
+		  _stop = $('#stop'),
+		  _reset = $('#reset'),
+		  _speech = $('#speech'),
+		  _translate = $('#translate');
+	_mic.addClass("mic_enabled");
+	_stop.addClass("mic_disabled");
+	_reset.addClass("mic_enabled");
+	_translate.addClass("mic_disabled");
 
-            stream = WatsonSpeech.SpeechToText.recognizeMicrophone({
-               token: token,
-               outputElement: '#speech' // CSS selector or DOM Element
-             });
-             _speech.css( "text-transform", "capitalize" );
-            stream.on('error', function(err) { console.log(err); });
-          });
-        }
-      });
-
-  _stop.on("click", () => {
-    console.log("Stopping text-to-speech service...");
-
-    if (!((typeof(stream) == "undefined") || (stream == null))) {
-		stream.stop(); 
+	_reset.on("click", () => {
+		_mic.addClass("mic_enabled");
+		_mic.removeClass("mic_disabled");
+		_translate.addClass("mic_disabled");
+		_translate.removeClass("mic_enabled");
+		const resetText = _speech.text().replace(/.*/, "Spoken output goes here");
+		_speech.html(resetText);
+		console.log("Speech cleared");
+	});
+	
+	_mic.on("click", function() {
+		let _className = this.className;
+		if(this.className == "mic_enabled") {
+			_mic.addClass("mic_disabled");
+			_mic.removeClass("mic_enabled");
+			_stop.addClass("mic_enabled");
+			_stop.removeClass("mic_disabled");
+			$.when($.get('/api/speech-to-text/token')).done(
+				(token) => {
+					stream = WatsonSpeech.SpeechToText.recognizeMicrophone({
+					token: token,
+					outputElement: '#speech' // CSS selector or DOM Element
+				});
+				_speech.css( "text-transform", "capitalize" );
+				stream.on('error', (err) => { console.log(err); });
+			});
+        } else {
+			console.log("Unable to complete request");
+		}
+	});
+		_stop.on("click", () => {
+			console.log("Stopping text-to-speech service...");
+			if (!((typeof(stream) == "undefined") || (stream == null))) { stream.stop(); }
+			_mic.addClass("mic_enabled");
+			_mic.removeClass("mic_disabled");
+			_stop.addClass("mic_disabled");
+			_stop.removeClass("mic_enabled");
+			_translate.addClass("mic_enabled");
+			_translate.removeClass("mic_disabled");
+		});
+		
+		_translate.on("click", () => {
+			let regex = /(^[^aeiou]*)(\w*\'?\w*)/ig;
+			let replacement = '$2-$1ay';
+			newSpeech = _speech.text().split(" ");
+			pigLatinSpeech = [];
+			newSpeech.forEach((element) => { pigLatinSpeech.push(element.replace(regex, replacement)); });
+			if (pigLatinSpeech[pigLatinSpeech.length - 1 ] == "-ay") {pigLatinSpeech.pop();}
+			translatedSpeech = pigLatinSpeech.join(" ");
+			_speech.html(translatedSpeech);
+			console.log("Translated to Pig Latin");
+		});
 	}
-    _mic.addClass("mic_enabled");
-    _mic.removeClass("mic_disabled");
-    _stop.addClass("mic_disabled");
-    _stop.removeClass("mic_enabled");
-  });
-}
